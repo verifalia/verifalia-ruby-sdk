f347b7adaf1f49980571c6864f6661fab486638a
Pramod <pramod@joshsoftware.com>
2015-11-21 23:43:00 +0530

    refs #14,
    + Updated EmailValidations#initialize to build resource with content_type
    + removed content_type from each api call moved to build_resource
    + updated specs for this change

--- lib/rest/email_validations.rb
+++ lib/rest/email_validations.rb
@@ -13,7 +13,7 @@ module Verifalia
       # The unique if of the Verifalia Email Validation resource
       #
       def initialize(config, account_sid, account_token, args = {})
-        @resource = RestClient::Resource.new "#{config[:host]}/#{config[:api_version]}/email-validations", account_sid, account_token
+        @resource = build_resource(config, account_sid, account_token)
         @unique_id = args[:unique_id] if args[:unique_id]
       end
 
@@ -29,7 +29,7 @@ module Verifalia
         data = emails.map { |email| { inputData: email }}
         content = { entries: data }.to_json
         begin
-          r = @resource.post content, content_type: :json
+          r = @resource.post content
           @unique_id = JSON.parse(r)["uniqueID"]
           @response = nil
           @error = nil
@@ -49,7 +49,7 @@ module Verifalia
         raise ArgumentError, 'You must call verify first or supply and uniqueId' unless @unique_id
         unless @response
           begin
-            r = @resource[@unique_id].get content_type: :json
+            r = @resource[@unique_id].get
             @response = JSON.parse(r)
             @error = nil
           rescue => e
@@ -68,7 +68,7 @@ module Verifalia
       def destroy
         raise ArgumentError, 'You must call verify first or supply and uniqueId' unless @unique_id
         begin
-          r = @resource[@unique_id].delete content_type: :json
+          r = @resource[@unique_id].delete
           @error = nil
           @response = nil
           @unique_id = nil
@@ -114,6 +114,16 @@ module Verifalia
             else
               @error = :internal_server_error
             end
+        end
+
+        def build_resource(config, account_sid, account_token)
+          api_url = "#{config[:host]}/#{config[:api_version]}/email-validations"
+          opts = {
+            user: account_sid, 
+            password: account_token, 
+            headers: { content_type: :json }
+          }
+          return RestClient::Resource.new api_url, opts
         end
     end
   end
